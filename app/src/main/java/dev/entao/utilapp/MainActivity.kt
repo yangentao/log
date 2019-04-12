b559b68389bbbb2a0007bc8c3b4e0e79bf12975b
yangentao <entaoyang@163.com>
2019-04-13 02:11:41 +0800

    2.0.1

--- app/src/main/java/dev/entao/utilapp/MainActivity.kt
+++ app/src/main/java/dev/entao/utilapp/MainActivity.kt
@@ -2,13 +2,14 @@ package dev.entao.utilapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dev.entao.log.logd
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        logd("Hello")
     }
 
 
--- app/src/main/java/dev/entao/utilapp/MyApp.kt
+++ app/src/main/java/dev/entao/utilapp/MyApp.kt
@@ -1,10 +1,12 @@
 package dev.entao.utilapp
 
 import android.app.Application
+import dev.entao.log.Yog
 
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Yog.init(this)
     }
 }
